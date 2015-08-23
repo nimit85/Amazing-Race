66a1a408e866256dc1b7e8d42d28520faf69aa78
nimit85 <nimit@HAL9000.hsd1.ma.comcast.net>
2015-08-23 16:30:33 -0400

    Segmentation code
    Code cleaned with call to stroke calculation code.
    New stroke calculation added. Returns vertical and horizontal strokes.

--- amazing_race.m
+++ amazing_race.m
@@ -1,5 +1,5 @@
 function amazing_race(imName)
-close all; clc;
+close all
 % read in file
 im = imread(imName);
 
@@ -34,66 +34,22 @@ for iter = 1 : CC.NumObjects
 end
 imNew(newPixList) = 1;
 
-% imwrite(imNew,'bwimage.tif');
+% get only the skeleton
 imNew = bwmorph(imNew,'skel');
-figure; imshow(imNew);
 
-CC = bwconncomp(imNew); 
-stats = regionprops(CC,'Image','PixelList');
-% 
-% figure;
-% for iter = 4 : size(stats,1)
-%     APix = stats(iter).PixelList;
-%     % find vertical strokes
-%     Vstr = vert_stroke(APix);
-%     % find horizontal strokes
-%     Hstr = horz_stroke(APix);
-%     % find diagonal strokes
-%     Dstr = diag_stroke(APix);
-% end
+[vertStrk, horzStrk] = strokeCalc(imNew);
 
-
-% 
-% 
 % L = bwlabel(imNew);
 % rgbIm = label2rgb(L);
 % figure; imshow(rgbIm);
-% 
+%
 
 % imwrite(rgbIm,'colorimage.tif');
 
 end
 
-% function cntr = vert_stroke(pixList)
-% cntr = 0; continueCtr = 0;
-% for iter = 1 : size(pixList,1) - 1
-%     if abs(pixList(iter+1,2) - pixList(iter,2)) == 1 && ...
-%             pixList(iter+1,1) == pixList(iter,1)
-%         continueCtr = continueCtr + 1;
-%     else
-%         if continueCtr >= 4
-%             cntr = cntr + 1;
-%         end
-%         continueCtr = 0;
-%     end
-% end
-% end
-% 
-% function cntr = horz_stroke(pixList)
-% cntr = 0; continueCtr = 0;
-% for iter = 1 : size(pixList,1) - 1
-%     if abs(pixList(iter+1,1) - pixList(iter,1)) == 1 && ...
-%             pixList(iter+1,2) == pixList(iter,2)
-%         continueCtr = continueCtr + 1;
-%     else
-%         if continueCtr >= 4
-%             cntr = cntr + 1;
-%         end
-%         continueCtr = 0;
-%     end
-% end
-% end
-% 
+
+
 % function cntr = diag_stroke(pixList)
 % cntr = 0; continueCtr = 0;
 % for iter = 1 : size(pixList,1) - 1
